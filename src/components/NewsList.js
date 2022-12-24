0040a6e52c05ef5787917de7b10ae51708542459
Ravina Dhamani <47536219+ravinadhamani@users.noreply.github.com>
2022-12-25 00:01:56 +0530

    Update NewsList.js

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -12,7 +12,7 @@ const NewsList = () => {
 
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=finance&apiKey=7fc3e528692a4b8885dbee9f1b997461`)
+            const response = await axios.get(`https://gnews.io/api/v4/search?q=example&token=74b3a0c17882f558376cbe68dd4b4490&lang=en`)
             setArticles(response.data.articles)
             console.log("response", response)
         }
@@ -24,7 +24,7 @@ const NewsList = () => {
             <Navbar />
             <div className={styles.newsMainDiv}>
                 <div>
-                    <p className={styles.latestNewsText}>Latest Finance News</p>
+                    <p className={styles.latestNewsText}>Latest News</p>
                 </div>
                 <div className={styles.newsApp}>
                     {articles.map(article => {
@@ -33,7 +33,7 @@ const NewsList = () => {
                                     title={article.title}
                                     description={article.description}
                                     url={article.url}
-                                    urlToImage={article.urlToImage}
+                                    urlToImage={article.image}
                                 />    
                         )
                     })}
